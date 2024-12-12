7ef181df5a110a4522072bf359832635cf2e618c
Jaco Kok <kokjaco2@gmail.com>
2024-12-12 11:37:05 +0200

    Mqtt config in appsettings.

--- Services/MqttService.cs
+++ Services/MqttService.cs
@@ -7,14 +7,19 @@ public class MqttService
 {
     private static readonly MqttFactory Factory = new();
     private readonly IMqttClient _mqttClient = Factory.CreateMqttClient();
-    private const string BrokerServer = "localhost";
+    private readonly IConfiguration _config;
+
+    public MqttService(IConfiguration config)
+    {
+        _config = config;
+    }
 
     public async Task ConnectMqttAsync()
     {
         if (_mqttClient.IsConnected) return;
 
         var options = new MqttClientOptionsBuilder()
-            .WithTcpServer(BrokerServer)
+            .WithTcpServer(_config["Config:MqttBroker"])
             .Build();
 
         await _mqttClient.ConnectAsync(options, CancellationToken.None);
